a5e6bb6960cbcd079418417ce7f8b71031df6494
mhsastratech13 <mhsastratech13astratech@polytechnic.astra.ac.id>
2026-03-11 11:45:47 +0700

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.topRow}>
+        <View style={[styles.box, styles.red]}></View>
+        <View style={[styles.box, styles.blue]}></View>
+        <View style={[styles.box, styles.green]}></View>
+        <View style={[styles.box, styles.yellow]}></View>
+      </View>
+
+      <View style={styles.bottom}></View>
+
     </View>
   );
 }
@@ -13,8 +21,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+
+  topRow: {
+    flexDirection: 'row',
+    height: 150,
+    backgroundColor: '#e8f0ed',
+    alignItems: 'flex-start', 
+  },
+
+  box: {
+    flex: 1,
+  },
+
+  red: {
+    backgroundColor: 'red',
+    height: 30,
+  },
+
+  blue: {
+    backgroundColor: 'blue',
+    height: 60,
+  },
+
+  green: {
+    backgroundColor: 'green',
+    height: 90,
+  },
+
+  yellow: {
+    backgroundColor: 'orange',
+    height: 120,
+  },
+
+  bottom: {
+    flex: 1,
+    backgroundColor: 'gray',
   },
 });
