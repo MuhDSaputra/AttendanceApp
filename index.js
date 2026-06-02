d5a9737cb494bf0f24268e08dfe050ae0104a593
MuhDSaputra <mrsaputra225@gmail.com>
2026-06-02 22:46:02 +0700

    twelve commit

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { registerRootComponent } from "expo";
 
 import App from "./App";
@@ -9,15 +8,3 @@ import App from "./App";
 // the environment is set up appropriately
 // registerRootComponent(MainApps);
 registerRootComponent(App);
-=======
-import { registerRootComponent } from 'expo';
-
-import MainApps from './App';
-// import App from './App';
-
-// // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// // It also ensures that whether you load the app in Expo Go or in a native build,
-// // the environment is set up appropriately
-// registerRootComponent(App);
-registerRootComponent(MainApps);
->>>>>>> 8053d70ca478b652d540e46e1b14af6cbe66ce3b
